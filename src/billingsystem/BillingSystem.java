59951224629ae4ae8927d0b43781fc5e8d3148b5
GourabRoy551 <gourabroysec553@gmail.com>
2020-07-01 11:28:00 +0600

    billingsystem

---
+++ src/billingsystem/BillingSystem.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package billingsystem;
+
+/**
+ *
+ * @author dhali
+ */
+public class BillingSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Billing_System().setVisible(true);
+            }
+        });
+    }
+    
+}
